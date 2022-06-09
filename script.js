3596020142ca7d7e57c70782b8fe7a273db1836f
Lorena Silva <lorena.silva@codeminer42.com>
2022-06-09 07:39:37 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let aluno = "lorena"
+let numero = 20
+
+let anoAtual = 2022
+
+let subtracao = anoAtual - numero
+console.log(subtracao)
+let adicao = subtracao + anoAtual
+
+console.log(adicao)
+
+console.log("Estou mexendo no console.log")
+
+let quartaFeira = true
+
+console.log(quartaFeira)
+
+console.log(typeof quartaFeira)
+
+
